8edb9d0d11e202bfa20ad3cab09eeef3795ba45a
sra <sra@DESKTOP-5UGJ5B3>
2017-04-17 15:54:26 +0600

    Test COmmit

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
+            //Just a test Commit.
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
