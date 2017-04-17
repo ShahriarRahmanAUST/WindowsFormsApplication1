6d1557224c1690beefab5ecda27a3db29dab9215
sra <sra@DESKTOP-5UGJ5B3>
2017-04-17 15:52:36 +0600

    Add project files.

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            XmlNodeList xmlnode;
+            int i = 0;
+            string str = null;
+            FileStream fs = new FileStream(@"C:\Users\sra\Downloads\metadata (1)", FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+            xmlnode = xmldoc.GetElementsByTagName("Product");
+            for (i = 0; i <= xmlnode.Count - 1; i++)
+            {
+                xmlnode[i].ChildNodes.Item(0).InnerText.Trim();
+                str = xmlnode[i].ChildNodes.Item(0).InnerText.Trim() + "  " + xmlnode[i].ChildNodes.Item(1).InnerText.Trim() + "  " + xmlnode[i].ChildNodes.Item(2).InnerText.Trim();
+                MessageBox.Show(str);
+            }
+        }
+    }
+}
